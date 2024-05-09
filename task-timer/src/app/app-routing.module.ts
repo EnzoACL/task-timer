aef858f7c97154727960a358901a30e75948b70e
Enzo <enzo.camilolado@gmail.com>
2024-05-09 19:17:05 +0200

    add routing to task component

--- task-timer/src/app/app-routing.module.ts
+++ task-timer/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskCardComponent } from './task-card/task-card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TaskCardComponent },  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
